661e4d323af4d94e9af9460aa2215ab7635a11be
Guillaume Quelen <quenechg@proton.me>
2026-03-20 23:30:36 +0100

    Vue Tournoi

---
+++ views/player_view.py
@@ -0,0 +1,12 @@
+class PlayerView:
+    def get_info(self):
+        first_name = input("Prénom")
+        last_name = input("Nom")
+        birth_date = input("Date de naissance")
+        national_id = input("Identifiant")
+        return first_name, last_name, birth_date, national_id
+    
+    def display_players(self, players):
+        for player in players:
+            print(player.first_name, player.last_name, player.national_id)
+        
